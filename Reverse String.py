464cc339795014d19358767fc27f4c45d71cb78e
Krishna Gunjan <146088165+Krishna-Gunjan@users.noreply.github.com>
2024-02-04 09:22:20 +0530

    Delete Reverse String.py

--- Reverse String.py
+++
@@ -1,8 +0,0 @@
-while True:
-    try:
-        my_string = input("Enter your string: ")
-        reversed_string = my_string[::-1]
-        print("The reversed string will be", reversed_string)
-        break
-    except:
-        print("Invalid Input! Please input a valid string.")
